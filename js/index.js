e4602204530aadbdfa0789dce99f08c37fff1398
MOISES-DARLAN <moisesdarlan440@gmail.com>
2025-10-11 11:06:23 -0400

    mostra a pagina inicial

---
+++ js/index.js
@@ -0,0 +1,22 @@
+ tailwind.config = {
+            darkMode: "class",
+            theme: {
+                extend: {
+                    colors: {
+                        "primary": "#007BFF",
+                        "background-light": "#F8F9FA",
+                        "background-dark": "#212529",
+                        "accent": "#20C997"
+                    },
+                    fontFamily: {
+                        "display": ["Space Grotesk", "sans-serif"]
+                    },
+                    borderRadius: {
+                        "DEFAULT": "0.25rem",
+                        "lg": "0.5rem",
+                        "xl": "0.75rem",
+                        "full": "9999px"
+                    },
+                },
+            },
+        }
